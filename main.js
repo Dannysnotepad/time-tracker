1b8effa93295eb5c7ceed392a3d25f22652a4248
Dannysnotepad <etimdnl41@gmail.com>
2023-08-25 05:48:05 +0100

    time tracker v0.2

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*
-*Time tracker
-*from
-*Dannys Notepad
-*/
+ *Time tracker
+ *from
+ *Dannys Notepad
+ */
 
 
 //Variables for greet display
@@ -14,57 +14,68 @@ const dateFormat = date.toDateString();
 const timeFormat = date.toLocaleTimeString();
 const documentBody = document.querySelector('body')
 
-//code to make the background color change depending on the time of the day
-if (greetHour >= 0 && greetHour <= 11) {
-  documentBody.style.backgroundColor = 'whitesmoke'
-} else if (greetHour >= 12 && greetHour <= 16) {
-  documentBody.style.backgroundColor = 'gold'
-  timeGreet.style.color = 'darkblue'
-  nameGreet.style.color = 'snow'
-} else if (greetHour >= 17) {
-  documentBody.style.backgroundColor = 'gray'
-  timeGreet.style.color = 'whitesmoke'
-}
 
 
-//Logic to greet with time
-if (greetHour >= 0 && greetHour <= 11) {
-  timeGreet.textContent = 'Good Morning 🌄!'
-} else if (greetHour >= 12 && greetHour <= 16) {
-  timeGreet.textContent = 'Good Afternoon 🌅!'
-} else if (greetHour >= 17) {
-  timeGreet.textContent = 'Good Evening 🌇!'
-}
+
+
 
 // display date
 let dateEl = document.getElementById('dateEl1')
 dateEl.textContent = 'Date: ' + dateFormat
 
+
 // display time
 let timeEl = document.getElementById('dateEl2')
 timeEl.textContent = 'Time: ' + timeFormat
 
+// UPDATE TIME
+setInterval(() => {
+  timeEl.textContent = 'Time: ' + (new Date()).toLocaleTimeString()
+  
+  
+  //Logic to greet with time
+  if (greetHour >= 0 && greetHour <= 11) {
+    timeGreet.textContent = 'Good Morning 🌄!'
+  } else if (greetHour >= 12 && greetHour <= 16) {
+    timeGreet.textContent = 'Good Afternoon 🌅!'
+  } else if (greetHour >= 17) {
+    timeGreet.textContent = 'Good Evening 🌇!'
+  }
+  
+  //code to make the background color change depending on the time of the day
+  if (greetHour >= 0 && greetHour <= 11) {
+    documentBody.style.backgroundColor = 'whitesmoke'
+  } else if (greetHour >= 12 && greetHour <= 16) {
+    documentBody.style.backgroundColor = 'gold'
+    timeGreet.style.color = 'darkblue'
+    nameGreet.style.color = 'snow'
+  } else if (greetHour >= 17) {
+    documentBody.style.backgroundColor = 'gray'
+    timeGreet.style.color = 'whitesmoke'
+  }
+  
+  
+}, 1000)
 
-formBody.addEventListener("submit", (e) =>{
+formBody.addEventListener("submit", (e) => {
   e.preventDefault()
 
   let userName = document.getElementById("userName")
-  
+
   if (userName.value == '') {
-    
+
     let errorMsg = document.getElementById('formError')
-    
+
     errorMsg.textContent = 'Enter name your name'
     setTimeout(() => {
       errorMsg.textContent = ''
     }, 5000)
-    
+
   } else {
     // perform operation with form input
     let nameGreeting = document.getElementById('nameGreet')
     nameGreeting.textContent = `Hello ${userName.value}!`
   }
-  
-  
-});
 
+
+});
